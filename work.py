5a960c464a2eae86a79c44730d083fe47133de64
hashirama <cosies-fades-0@icloud.com>
2025-11-06 17:57:29 +0300

    watchdog

--- work.py
+++ work.py
@@ -1,5 +1,5 @@
-from time import time
-from numpy.random import random_sample
+import threading
+import time
 
 from tango import DevState
 from tango.server import Device, attribute, command
@@ -100,36 +100,41 @@ class _BUK_M(Device):
 # ########################################################################################
 # ########################################################################################
 class BUK_M(_BUK_M): # ModbusID 0
-	DEVICE_CLASS_DESCRIPTION = "блок управления и коммутации БУК-М"
-
 	_REGISTER_STATUS_WORD = 300001
 	_REGISTER_ERROR_WORD = 300002
 	_REGISTER_ACCIDENT_WORD = 300003
 	_REGISTER_COMMAND_WORD = 400001
 	_REGISTER_COMMAND_PULSE = 400002
 
+# ########################################################################################
+	DEVICE_CLASS_DESCRIPTION = "блок управления и коммутации БУК-М"
+
+	class WatchDog:
+		timer : threading.Timer
+		interval : int 
+		callback = None
+
+	_inner_watch_dog : WatchDog # периодически посылает запросы по модбас
+	_outer_watch_dog : WatchDog # периодически проверяет, давно ли клиенты просили присылать данные (если давно, то отписывает)
+
+# ########################################################################################
 	def init_device(self):
 		super().init_device()
 
-		print("=== ТЕСТ status.getter ===")
-
-		print(self.status_read())
-
-
 # ########################################################################################
-	# status = attribute(
+	# status = attribute( #TODO
 	# 	label="status",
 	# 	dtype=str,
 	# 	doc="Статусное слово"
 	# )
 	# @status.getter 
 	def status_read(self):
-		result = self._read_input_registers(300001, 1)
+		result = self._read_input_registers(self._REGISTER_STATUS_WORD, 1)
 
 		if result is None:
-			return "Ошибка чтения статуса"
+			return "Ошибка чтения слова статуса"
 		
-		status_bits = format(result[0], '016b')
+		status_bits = format(result[0], '016b')[::-1]
 		print(status_bits)
 		status_map = {
 			0: "Инициализация контроллера",
@@ -141,7 +146,7 @@ class BUK_M(_BUK_M): # ModbusID 0
 			6: "Циклограмма выполняется",
 			15: "Запись команд запрещена"
 		}
-		
+
 		for bit_index, message in status_map.items():
 			if status_bits[bit_index] == '1':
 				return message
@@ -150,78 +155,69 @@ class BUK_M(_BUK_M): # ModbusID 0
 	
 
 # ########################################################################################
-	def _do_command(self, command_code ):
+	def _do_command(self, addr,  command_code ):
 		response = self.modbus_client.write_register(
-				address=self._REGISTER_COMMAND_WORD - self._OFFSET_HOLDING_REGISTER,
+				address= addr - self._OFFSET_HOLDING_REGISTER,
 				value=command_code,
 				device_id=self.modbus_id
 			)
 		return self._process_response(response)
 	
-	_AUX_16_ZEROS_LIST = ['0'] * 16
-
-	_BIN_LIST_STOP = 	_AUX_16_ZEROS_LIST.copy() 	
-	_BIN_LIST_STOP[1]	= '1'
-
-	_BIN_LIST_GET_CYCLOGRAMM = _AUX_16_ZEROS_LIST.copy() 	
-	_BIN_LIST_GET_CYCLOGRAMM[2] = '1'
-
-	_BIN_LIST_CANCEL_CYCLOGRAMM = _AUX_16_ZEROS_LIST.copy() 		
-	_BIN_LIST_CANCEL_CYCLOGRAMM[3] = '1'				
-
-	_BIN_LIST_RESET_INITIALIZATION = 	_AUX_16_ZEROS_LIST.copy() 	
-	_BIN_LIST_RESET_INITIALIZATION[5] = '1'
-
-	_BIN_LIST_ACKNOWLEDGE_ERROR = _AUX_16_ZEROS_LIST.copy() 	
-	_BIN_LIST_ACKNOWLEDGE_ERROR[6] = '1'
-						   
-	_BIN_LIST_ACKNOWLEDGE_ACCIDENT = 	_AUX_16_ZEROS_LIST.copy() 	
-	_BIN_LIST_ACKNOWLEDGE_ACCIDENT[7] = '1'
-
-	_BIN_LIST_RESET_INITIALIZATION_INCOMPLETE = _AUX_16_ZEROS_LIST.copy() 	
-	_BIN_LIST_RESET_INITIALIZATION_INCOMPLETE[13] = '1'
-
-	def _bin_list_to_int(bin_list):
-		return int(''.join(bin_list), 2)
-
+	_BIN_CODE_STOP = 1 << 1
+	_BIN_CODE_GET_CYCLOGRAMM = 1 << 2
+	_BIN_CODE_CANCEL_CYCLOGRAMM = 1 << 3
+	_BIN_CODE_RESET_INITIALIZATION = 1 << 5
+	_BIN_CODE_ACKNOWLEDGE_ERROR = 1 << 6
+	_BIN_CODE_ACKNOWLEDGE_ACCIDENT = 1 << 7
+	_BIN_CODE_RESET_INITIALIZATION_INCOMPLETE = 1 << 13
 
 	@command(doc_in="???#TODO")
 	def stop(self):
-		return self._do_command(self._bin_list_to_int(self._BIN_LIST_STOP))
+		return self._do_command(self._BIN_CODE_STOP)
 		
 # ########################################################################################
 	@command
 	def get_cyclogram(self):
-		return self._do_command(self._bin_list_to_int(self._BIN_LIST_GET_CYCLOGRAMM))
+		return self._do_command(self._REGISTER_COMMAND_WORD, self._BIN_CODE_GET_CYCLOGRAMM)
 	
 # ########################################################################################
 	@command
 	def cancel_cyclogram(self):
-		pass # TODO
+		return self._do_command(self._REGISTER_COMMAND_WORD, self._BIN_CODE_CANCEL_CYCLOGRAMM)
+	
 # ########################################################################################
 	@command
 	def reset_initialization(self):
-		pass # TODO
+		return self._do_command(self._REGISTER_COMMAND_WORD, self._BIN_CODE_RESET_INITIALIZATION)
+	
 # ########################################################################################
 	@command
 	def acknowledge_error(self):
-		pass # TODO
+		return self._do_command(self._REGISTER_COMMAND_WORD, self._BIN_CODE_ACKNOWLEDGE_ERROR)
+	
 # ########################################################################################
 	@command
 	def acknowledge_accident(self):
-		pass # TODO
+		return self._do_command(self._REGISTER_COMMAND_WORD, self._BIN_CODE_ACKNOWLEDGE_ACCIDENT)
+	
 # ########################################################################################
 	@command
 	def reset_initialization_incomplete(self):
-		pass # TODO
+		return self._do_command(self._REGISTER_COMMAND_WORD, self._BIN_CODE_RESET_INITIALIZATION_INCOMPLETE)
+	
 # ########################################################################################
+
+	_CODE_ENABLE_PULSE_MODE = 1
+	_CODE_DISABLE_PULSE_MODE = 2
+
 	@command
 	def enable_pulse_mode(self):
-		pass # TODO
+		return self._do_command(self._REGISTER_COMMAND_PULSE, self._CODE_ENABLE_PULSE_MODE)
+	
 # ########################################################################################
 	@command
 	def disable_pulse_mode(self):
-		pass # TODO
+		return self._do_command(self._REGISTER_COMMAND_PULSE, self._CODE_DISABLE_PULSE_MODE)
 
 # ########################################################################################
 	errors = attribute(
@@ -231,51 +227,182 @@ class BUK_M(_BUK_M): # ModbusID 0
 	)
 	@errors.read
 	def errors_read(self):
-		errors_bits = "110101011010110" # TODO
+		result = self._read_input_registers(self._REGISTER_ERROR_WORD, 1)
 
-		response_str = ""
+		if result is None:
+			return "Ошибка чтения слова ошибок"
 
-		if errors_bits[0]: #TODO
-			response_str = "TFTP-сервер недоступен"
-		if errors_bits[1]:
-			response_str = "Ошибка открытия файла конфигурации"
-		if errors_bits[2]:
-			response_str = "Ошибка загрузки файла конфигурации"
-		if errors_bits[3]:
-			response_str = "Ошибка при инициализации модулей или устройств"
-		if errors_bits[4]:
-			response_str = "Ошибка установки соединения"
-		if errors_bits[5]:
-			response_str = "Ошибка загрузки циклограммы"
-		if errors_bits[6]:
-			response_str = "Ошибка обработки циклограммы"
-		if errors_bits[7]:
-			response_str = "Ошибка при выполнении циклограммы"
-		if errors_bits[8]:
-			response_str = "Ошибка"
-		if errors_bits[9]:
-			response_str = "Потеря связи с сервером"
-		if errors_bits[10]:
-			response_str = "Авария модуля"
-		if errors_bits[11]:
-			response_str = "Неверная команда"
-		if errors_bits[12]:
-			response_str = "Авария"
-		if errors_bits[13]:
-			response_str = "Ошибка IRIG"
+		errors_bits = format(result[0], '016b')[::-1]
+		print(f"Слово ошибок: {errors_bits}")
+
+		errors_map = {
+			0: "TFTP-сервер недоступен",
+			1: "Ошибка открытия файла конфигурации",
+			2: "Ошибка загрузки файла конфигурации",
+			3: "Ошибка при инициализации модулей или устройств",
+			4: "Ошибка установки соединения",
+			5: "Ошибка загрузки циклограммы", 
+			6: "Ошибка обработки циклограммы",
+			7: "Ошибка при выполнении циклограммы",
+			8: "Ошибка",
+			9: "Потеря связи с сервером",
+			10: "Авария модуля",
+			11: "Неверная команда",
+			12: "Авария",
+			13: "Ошибка IRIG"
+		}
+
+		active_errors = []
+		for bit_index, message in errors_map.items():
+			if errors_bits[bit_index] == '1':
+				active_errors.append(message)
+
+		return "; ".join(active_errors) if active_errors else "Ошибок нет"
+
+	# ########################################################################################
+	accidents = attribute(
+		label="accidents",
+		dtype=str,
+		doc="Слово аварий"
+	)
+	@accidents.read
+	def _(self):
+		return "функция слова аварий пока не поддерживается"
+
+
+# ########################################################################################
+# ########################################################################################
+
+
+class BUK_M1_CORRECTOR_CURRENT_SUPPLIER(BUK_M): # ModbusID 1-8
+	_REGISTER_STATUS_WORD = 300001
+	_REGISTER_ERROR_WARNING = 300002
+	# _REGISTER_OUTPUT_CURRENT_FLOAT = 
+	# _REGISTER_ = 
+	# _REGISTER_ = 
+	# _REGISTER_ = 
+	# _REGISTER_ = 
+
+# ########################################################################################
+	DEVICE_CLASS_DESCRIPTION = "Источники тока корректоров"
+
+# ########################################################################################
+	def init_device(self):
+		super().init_device()
+		print(self.status_read())
+# ########################################################################################
+	# status = attribute(
+	# 	label="status",
+	# 	dtype=str,
+	# 	doc="Статус источника тока"
+	# )
+	# @status.read
+	def status_read(self):
+		result = self._read_input_registers(300001, 1)
+
+		if result is None:
+			return "Ошибка чтения статуса источника"
+
+		status_bits = format(result[0], '016b')[::-1]
+		print(f"Статус источника: {status_bits}")
+
+		# Состояния по битам 0 и 1
+		state_map = {
+			(0, 0): "отключен",
+			(1, 0): "штатная работа", 
+			(0, 1): "останов в безопасном режиме",
+			(1, 1): "режим прямого управления ШИМ"
+		}
+
+		# Определяем основное состояние
+		bit_0 = 1 if status_bits[0] == '1' else 0  # Бит 0 (младший)
+		bit_1 = 1 if status_bits[1] == '1' else 0  # Бит 1
+		state_key = (bit_1, bit_0)
+		
+		response_str = state_map.get(state_key, "неизвестное состояние")
+
+		# Добавляем статус инициализации (бит 2)
+		bit_2 = status_bits[2] == '1'  
+
+		if bit_2:
+			response_str += ", инициализирован"
+		else:
+			response_str += ", неинициализирован"
 
 		return response_str
-
-
-
+	
+# ########################################################################################
+	error_warning = attribute(
+		label="error_warning",
+		dtype=str,
+		doc="Значение кода ошибки/предупреждения"
+	)
+	@error_warning.read
+	def _(self):
+		return 1 # TODO
+	
+# ########################################################################################
+	output_current_float = attribute(
+		label="output_current_float",
+		dtype=float,
+		doc="Значение выходного тока (float)"
+	)
+	@output_current_float.read
+	def _(self):
+		return 1 # TODO
+# ########################################################################################
+	load_current_float = attribute(
+		label="load_current_float",
+		dtype=float,
+		doc="Значение тока в нагрузке (float)"
+	)
+	@load_current_float.read
+	def _(self):
+		return 1 # TODO
+# ########################################################################################
+	load_voltage_float = attribute(
+		label="load_voltage_float",
+		dtype=float,
+		doc="Значение напряжения в нагрузке (float)"
+	)
+	@load_voltage_float.read
+	def _(self):
+		return 1 # TODO
+# ########################################################################################
+	temp_modulator_transistors_float = attribute(
+		label="temp_modulator_transistors_float",
+		dtype=float,
+		doc="Значение температуры транзисторов модулятора (float)"
+	)
+	@temp_modulator_transistors_float.read
+	def _(self):
+		return 1 # TODO
+# ########################################################################################
+	temp_throttle_float = attribute(
+		label="temp_throttle_float",
+		dtype=float,
+		doc="Значение температуры дросселя (float)"
+	)
+	@temp_throttle_float.read
+	def _(self):
+		return 1 # TODO
+# ########################################################################################
+	setpoint_output_current_float = attribute(
+		label="setpoint_output_current_float",
+		dtype=float,
+		doc="Значение уставки выходного тока (float)"
+	)
+	@setpoint_output_current_float.read
+	def _(self):
+		return 1 # TODO
 
 
 if __name__ == "__main__":
-	BUK_M.host = "10.10.9.89"
-	BUK_M.port = 502
-	BUK_M.modbus_id = 0
+	BUK_M1_CORRECTOR_CURRENT_SUPPLIER.host = "10.10.9.89"
+	BUK_M1_CORRECTOR_CURRENT_SUPPLIER.port = 502
+	BUK_M1_CORRECTOR_CURRENT_SUPPLIER.modbus_id = 0
 
-	BUK_M.run_server()
+	BUK_M1_CORRECTOR_CURRENT_SUPPLIER.run_server()
 
 
 
